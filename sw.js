b7f4b736e8dc8167297c7e848079a9c8e11febb5
soumparabens <soumparabens@users.noreply.github.com>
2026-09-06 17:20:16 -0300

    App So um Parabens v06/09-c32

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Estratégia "rede primeiro" (network-first): com internet baixa a versão nova;
 // sem internet usa o cache. Atualiza sozinho ao detectar um sw.js novo.
 // >>> Ao subir uma atualização, troque a data abaixo para forçar renovação. <<<
-const CACHE = 'soumparabens-2026-09-02';
+const CACHE = 'soumparabens-v06-09-c32';
 
 self.addEventListener('install', (e) => { self.skipWaiting(); });
 self.addEventListener('activate', (e) => {
